e74ca159367b92d8e80cfcf23dd18b51588ceed9
Sabit-Arysbekov <127682595+sabit-arysbekovv@users.noreply.github.com>
2023-03-13 00:32:42 +0600

    uploading

---
+++ sc.js
@@ -0,0 +1,17 @@
+let insert = document.getElementById('insert')
+
+document.addEventListener('keydown',(event)=>{
+    insert.innerHTML = `
+    <div class = "key">
+    ${event.key === '' ? 'space': event.key}
+    <small>event.key</small>
+    </div>
+    <div class = "key">
+    ${event.keyCode}
+    <small>event.keycode</small>
+    </div>
+    <div class = "key">
+    ${event.code}
+    <small>event.code</small>
+    </div>`
+})
